04d2287b4394b37c995feadc0f912834e4ff153b
chandana M <chandu11288@gmail.com>
2018-04-18 17:47:09 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,13 +4,11 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-
   end
   def add_song(song)
     @songs << song
     @@song_count += 1
     song.artist = self
-
   end
   def add_song_by_name(name)
     song = Song.new(name)
